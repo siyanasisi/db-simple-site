12044eeeb543bcd98b091822841789ff6e140afa
Siyana Racheva <119444284+siyanasisi@users.noreply.github.com>
2024-11-10 18:54:12 +0200

    db test

--- completeApp/app.py
+++ completeApp/app.py
@@ -7,13 +7,19 @@ app.secret_key = "secrettttttkeyyyyyyyyyy123456789"
 
 @app.route("/")
 def index():
-    return "<h1>starter app</h1>"
+    data = get_db()
+    return data[0]
 
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect('grocery_list.db')
-    return db
+        cursor = db.cursor()
+        cursor.execute("select name from groceries")
+        all_data = cursor.fetchall()
+        all_data = [str(val[0]) for val in all_data]
+
+    return all_data
 
 @app.teardown_appcontext 
 def close_connection(exception):
